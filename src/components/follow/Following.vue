2abec544993866134ba04550146300046c0cc688
Noah <23338687+enjay27@users.noreply.github.com>
2021-11-26 00:18:30 +0900

    Follow 버튼 잘 작동하도록 수정

--- src/components/follow/Following.vue
+++ src/components/follow/Following.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="following">
-    <br />
-    <div id="users" class="row">
-      <div class="col-md-1" />
-      <div class="col-md-6">
-        <div class="card" v-for="(item, index) in users" :key="index">
-          <div class="card-body">
-            <h4 class="card-title">{{ item.display_name }}</h4>
-            <p class="card-text">{{ item.email }}</p>
-            <a
-              style="cursor: pointer"
-              @click="unfollow(item.member_id)"
-              class="btn btn-danger"
-              >Unfollow</a
-            >
+    <div class="container">
+      <br />
+      <div id="users" class="row">
+        <div class="col-md-1" />
+        <div class="col-md-6">
+          <div class="card" v-for="(item, index) in users" :key="index">
+            <div class="card-body">
+              <h4 class="card-title">{{ item.display_name }}</h4>
+              <p class="card-text">{{ item.email }}</p>
+              <a
+                v-show="!item.follow"
+                style="cursor: pointer"
+                @click="follow(index)"
+                class="btn btn-primary"
+                >Follow</a
+              >
+              <a
+                v-show="item.follow"
+                style="cursor: pointer"
+                @click="unfollow(index)"
+                class="btn btn-danger"
+                >Unfollow</a
+              >
+            </div>
           </div>
         </div>
       </div>
@@ -41,6 +51,16 @@ export default {
       });
   },
   methods: {
+    follow(index) {
+      console.log("follow " + this.users[index].member_id);
+      var params = {
+        memberId: this.user.member_id,
+        followId: this.users[index].member_id,
+      };
+      axios.post("http://localhost:8077/user/follow", params);
+      this.users[index].follow = true;
+      this.$emit("follow", 1);
+    },
     unfollow(index) {
       console.log("unfollow " + this.users[index].member_id);
       axios.delete("http://localhost:8077/user/follow", {
@@ -50,6 +70,7 @@ export default {
         },
       });
       this.users[index].follow = false;
+      this.$emit("follow", -1);
     },
   },
 };
