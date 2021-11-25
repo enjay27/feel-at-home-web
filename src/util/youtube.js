e33ea5b3d33d360f004738fbfa6ee9038575d7bf
Noah <23338687+enjay27@users.noreply.github.com>
2021-11-25 09:11:59 +0900

    youtube music list 서버에 저장

--- src/util/youtube.js
+++ src/util/youtube.js
@@ -1,12 +1,17 @@
 //YOUTUBE DATA API v3. Search
 //help for params : https://developers.google.com/youtube/v3/docs/search/list#--
+
+import axios from "axios";
+
+// import axios from "axios";
+
 var arrID = [];
 var arrTitle = [];
 var request = require("request");
 var optionParams = {
   q: "happy+joyful music",
   part: "snippet",
-  key: "AIzaSyCZ4fo41DfflV-WKzv5y3Zq6kLGgNqglzM",
+  key: "AIzaSyDlkFwFbKkX7bRRReJ28U2xGjGsyrxBZbY",
   type: "video",
   maxResults: 10,
   regionCode: "KR",
@@ -21,20 +26,53 @@ for (var option in optionParams) {
   url += option + "=" + optionParams[option] + "&";
 }
 
-//url의마지막에 붙어있는 & 정리
+// //url의마지막에 붙어있는 & 정리
 url = url.substr(0, url.length - 1);
+
+// axios
+//   .get("https://www.googleapis.com/youtube/v3/search", optionParams, {
+//     headers: {
+//       "Content-Type": `application/json`,
+//     },
+//   })
+//   .then((response) => {
+//     console.log(response);
+//   });
+// axios.post("http://localhost:8077/music/recommend",)
 
 request(url, function (err, res, body) {
   //console.log(body);
 
   //json형식을 서버로 부터 받음
   var data = JSON.parse(body).items;
+  var musics = [];
   for (var content in data) {
     //youtube downloader에 videoId 넘기면 됨.
-    console.log(data[content].snippet.title + " : " + data[content].id.videoId);
+    // console.log(data[content].snippet.title + " : " + data[content].id.videoId);
+    // console.log(data[content].snippet.thumbnails);
     arrID[content] = data[content].id.videoId;
     arrTitle[content] = data[content].snippet.title;
+    var music = {
+      title: data[content].snippet.title,
+      youtube_id: data[content].id.videoId,
+      description: data[content].snippet.description,
+      thumbnail: data[content].snippet.thumbnails.high.url,
+    };
+    musics.push(music);
+    console.log(musics);
   }
+  console.log("youtube 1");
+  console.log(data);
+  axios
+    .post("http://localhost:8077/music/recommend", musics)
+    .then((response) => {
+      console.log(response);
+    });
+  // axios
+  //   .post("http://localhost:8077/music/recommend", musics)
+  //   .then((response) => {
+  //     console.log(response);
+  //   });
 });
 
 const test = { arrID, arrTitle, optionParams };
